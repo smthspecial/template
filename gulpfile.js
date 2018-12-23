db2b361bda2e07790df2dc55006ad5bedefc8795
smthspecial <piruvat1@gmail.com>
2018-12-23 12:46:34 +0300

    imagemin added to gulp, babel presets added

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,17 @@ var browserSync = require("browser-sync").create();
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var del = require('del');
+var imagemin = require('gulp-imagemin');
 
 
 gulp.task('css', function(){
     return gulp.src("./src/scss/**/*.scss")
     .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
-    .pipe(postcss([ autoprefixer() ]))
-    .pipe(concat('all.css'))
+    .pipe(postcss([autoprefixer({
+             browsers: ['last 2 versions'],
+             cascade: false})]))
+    // .pipe(concat('all.css'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./public/css'))
     .pipe(browserSync.stream());
@@ -25,7 +28,10 @@ gulp.task('css', function(){
 gulp.task('js', function(){
     return gulp.src('./src/js/**/*.js')
     .pipe(sourcemaps.init())
-    .pipe(babel())
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(concat("app.js"))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./public/js'))
     .pipe(browserSync.stream());
@@ -42,21 +48,33 @@ gulp.task('js', function(){
 //     .pipe(browserSync.stream());
 // })
 
+gulp.task('img', function(){
+    return gulp.src('./src/img/**/*.*')
+    .pipe(sourcemaps.init())
+    .pipe(imagemin())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/img'))
+    .pipe(browserSync.stream());
+})
 
 
-gulp.task("build", gulp.series('css', 'js', 
+
+gulp.task("build", gulp.series('css', 'js', 'img' 
 // 'nextTask'
 ));
 
 gulp.task('watch', function(){
     gulp.watch('./src/scss/**/*.scss', gulp.series('css'));
     gulp.watch('./src/js/**/*.js', gulp.series('js'));
+    gulp.watch('./src/img/**/*.*', gulp.series('img'));
     // gulp.watch('./src/**/*.*', gulp.series('nextTask'));
 });
 
 gulp.task('server', function(){
     browserSync.init({
-        server: {baseDir: 'public/'}
+        server: {baseDir: 'public/',
+                 browser: "google chrome"}
+        
     });
 
     browserSync.watch("public/**/*.*").on('change', browserSync.reload);
@@ -66,41 +84,3 @@ gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'server')));
 // gulp.task('build:prod', gulp.series())
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//sass.compiler = require("node-sass");
-
-// gulp.task("brs", function() {
-//   browserSync.init({
-//     server: {
-//       baseDir: "./public"
-//     }
-//   });
-// //   gulp.watch("./public").on("change", browserSync.reload);
-//   gulp.watch("./src/**/*.scss", gulp.series("sass"));
-// });
-
-// gulp.task("sass", function() {
-//   return gulp
-//     .src("./src/scss/**/*.scss")
-//     .pipe(sass().on("error", sass.logError))
-//     .pipe(gulp.dest("./public/css"))
-//     .pipe(browserSync.stream());
-// });
-
-// gulp.task("watch", gulp.series('sass', 'brs'), function() {
-//   gulp.watch("./src/**/*.scss",).on('change', gulp.series('sass'));
-// });
